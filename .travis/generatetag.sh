8e0d0e8828dd1d649a9ab30368520abb94842e1c
paulfantom <pawel@krupa.net.pl>
2018-01-01 13:45:42 +0100

    test better semver

--- .travis/generatetag.sh
+++ .travis/generatetag.sh
@@ -2,7 +2,7 @@
 
 git config --global user.email "soibot@sointeractive.pl"
 git config --global user.name "soi-bot"
-GIT_TAG=$([[ "$TRAVIS_COMMIT_MESSAGE" =~ ("Merge pull request".*/feature.*) ]] && git semver --next-minor || git semver --next-patch )
+GIT_TAG=$([[ "$TRAVIS_COMMIT_MESSAGE" =~ ("Merge pull request".*[feature].*) ]] && git semver --next-minor || git semver --next-patch )
 echo $GIT_TAG
 git tag $GIT_TAG -a -m "Generated tag from TravisCI for build $TRAVIS_BUILD_NUMBER"
 GIT_URL=$(git config --get remote.origin.url)
